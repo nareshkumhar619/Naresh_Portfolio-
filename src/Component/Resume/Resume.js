6bdeb56838fefd3ec4e563168bfb7be3d4517570
Naresh Kumhar <122338149+nareshkumhar619@users.noreply.github.com>
2023-08-21 20:13:15 +0530

    Update Resume.js
    add

--- src/Component/Resume/Resume.js
+++ src/Component/Resume/Resume.js
@@ -5,10 +5,10 @@ import Particles from "../Particles"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import NareshR from "../../Assets/NareshR.pdf";
+import Nresume from "../../Assets/Nresume.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resume = NareshR;
+const resume = Nresume;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -28,11 +28,7 @@ function ResumeNew() {
           }}
         >
   </div>
-        <Container className="resume">
-          <h3>Click on Download To Get My Resume</h3>
-        </Container>
-
-        <div
+       <div
           style={{
             justifyContent: "center",
             position: "relative",
